7b7f205148425e8d216edcf214df5facb5cdbf99
Anton-Pan <91840673+Anton-Pan@users.noreply.github.com>
2023-12-29 00:52:21 -0800

    added toggling between full arrows and shorter arrows with distance text > moved global variables to the top of the file > added generator and clear functions for the dsitance text > implemented distance text to completion

--- main.py
+++ main.py
@@ -7,15 +7,27 @@ from PIL import Image, ImageTk
 from image_processing import getImageJSON
 from virtual_infrared_sensor import getDistances
 
-#Floorplan START
+# Floorplan START
 floorplan = input("Input floorplan filename: ")
 floorplanPath = 'floorplans/' + floorplan + '.png'
 floorJSON = getImageJSON(floorplanPath)
-#Floorplan END
+# Floorplan END
 
 # roomba input 'spawnpoint'
 startPosX = input("Input starting x-coord: ")
 startPosY = input("Input starting y-coord: ")
+
+# contains the toggle value of whether the user wants to see coordinates visible next to the arrow endpoints
+global showDistanceNumbers
+showDistanceNumbers = True
+
+# list to keep track of coordinate boxes
+global distanceTextList
+distanceTextList = []
+
+# list to keep track of line ids to delete to clear canvas
+global lineList
+lineList = []
 
 # posXY is updated by movement functions throughout program and is needed as a parameter by other functions
 global posXY
@@ -28,7 +40,7 @@ roombaRotation = 0
 def check_for_wall(dx, dy):
     # check if the pixel at the position i want to move to contains a wall, by checking floorJSON with the given coordinates.
     x, y = canvas.coords(roombaObj)
-    x, y = map(int, (x + dx + 10, y + dy + 10)) # adds the future/requested coordinates to the current coordinates
+    x, y = map(int, (x + dx + 10, y + dy + 10))  # adds the future/requested coordinates to the current coordinates
 
     '''
     note:   the + 10 is to account for the fact that the origin of the image is the top left, and the image is 21x21, 
@@ -36,8 +48,8 @@ def check_for_wall(dx, dy):
 
             the origin (0,0) is top left, and the positive x-dir is to the right whereas the POSITIVE y-dir is DOWN
     '''
-    
-    if floorJSON.get(f"({x}, {y})") == "W": # checks if the new coordinates contain a wall
+
+    if floorJSON.get(f"({x}, {y})") == "W":  # checks if the new coordinates contain a wall
         return True
     else:
         return False
@@ -45,7 +57,7 @@ def check_for_wall(dx, dy):
 def update_roomba_rotation(angle_degrees):
     global roombaRotation
     roombaRotation += angle_degrees
-    roombaRotation %= 360 # Ensure the angle stays within 0 to 359 degrees
+    roombaRotation %= 360  # Ensure the angle stays within 0 to 359 degrees
 
 def create_movable_object(x, y):
     # Create an image on the canvas
@@ -58,7 +70,7 @@ def create_movable_object(x, y):
 def move(event, dx, dy):
     dx = round(dx)
     dy = round(dy)
-    #print(dx, dy)
+    # print(dx, dy)
     if not check_for_wall(dx, dy):
         canvas.move(roombaObj, dx, dy)
 
@@ -91,6 +103,7 @@ def rotate_left(event):
 
 def rotate_right(event):
     rotate_image(-45)  # Rotate 45 degrees to the right
+
 # Movement/Rotations END
 
 # Load your apartment layout image
@@ -124,73 +137,87 @@ lambda is required below because in order to use tkinter's 'bind' method, there 
 so we use lambda to combine the 3 required parameters for getDistances() into one lamba function with the 'event'
 parameter while still handing off currentPos, facingAngle, and posDict to getDistances(), which are the parameters
 it needs as can be seen in virtual_infrared_sensor.py
-''' # this was changed after using a wrapper function, now lambda is just there for funsies, can remove if/when optimizing
+'''  # this was changed after using a wrapper function, now lambda is just there for funsies, can remove if/when optimizing
 
 # ensure parameters are correct for getDistances()
 posXY = canvas.coords(roombaObj)
 
 # wrapper function to get distances using virtual_infrared_sensor.py
-def wrapperFunction():
+def drawTracersAndDistancesWrapper():
     global distanceData
     distanceData = getDistances(posXY, roombaRotation, floorJSON)
     # distanceData is a list with 3 elements, the first is the tuple of lengths from the 3 sensors (left, front, right, in that order),
     # the second is a list (inside the first list) with all lengths from walls in the 8 cardinal directions, the third and last elements
     # is the coords of each of the 8 endpt pixels to where lines will be drawn
 
-    drawTracers()
-
-# to keep track of line ids to delete to clear canvas
-global lineList
-lineList = []
-
-# switch distances labels on/off
-global distLabelsOn
-distLabelsOn: bool = False 
-
-# flipflop function for toggling between lines or lines + label values
-def distLabelsToggle():
-    global distLabelsOn
-    distLabelsOn = not(distLabelsOn)
-
-def drawTracers():
     clearTracers()
-    # uses pixel endpts to draw lines to visualize the lengths each number represents
-    global distLabelsOn
+    clearDistanceBoxes()
+    if showDistanceNumbers:
+        drawTracers("short")
+        drawDistanceBoxes()
+    else:
+        drawTracers("short")
 
-    x = 0
-    for lineEndPts in distanceData[2]:
-        # Coordinates of the line's start and end points
-        x1, y1 = (int(posXY[0]) + 10), (int(posXY[1]) + 10) # Starting point (x1, y1)
-        x2, y2 = lineEndPts[0], lineEndPts[1] # Ending point (x2, y2)
-        
-        delta = 25
-        if ((distLabelsOn) and (((distanceData[1])[x]) > 25)):
-            if (x2 > x1):
-                x2 -= delta
-            elif (x2 < x1):
-                x2 += delta
-            if (y2 > y1):
-                y2 -= delta
-            elif (y2 < y1):
-                y2 += delta
-            
-        # Draw a line from (x1, y1) to (x2, y2)
-        lineID = canvas.create_line(x1, y1, x2, y2, fill = "blue", width = 1, dash = (4, 2), arrow = tk.LAST)
-        lineList.append(lineID)
-        x += 1
+def toggleDistanceText():
+    global showDistanceNumbers
+    showDistanceNumbers = not showDistanceNumbers
+
+# clears each coordinate box
+def clearDistanceBoxes():
+    for coordText in distanceTextList:
+        canvas.delete(coordText)
+
+def drawDistanceBoxes():
+    cardinalDirections = ["n", "nw", "w", "sw", "s", "se", "e", "ne"]
+    distList = distanceData[1]
+    xyList = distanceData[2]
+    for index in range(0, 8):  # both distList and cardinalDirections have a length of 8
+        x, y = xyList[index][0], xyList[index][1]  # Ending point (x, y)
+        dist = distList[index]
+        boxAnchor = cardinalDirections[index]
+        # add distance text for each tracer
+        coordText = canvas.create_text(x, y, text=dist, anchor=boxAnchor)
+        distanceTextList.append(coordText)
 
 # clears ANY and ALL existing lines/tracers
 def clearTracers():
     for line in lineList:
         canvas.delete(line)
 
-# Bind the 'g' key using lambda to get distances anytime g is pressed
-# tuple printed in terminal is in the order of (distance between left sensor and left wall, 
-# distance from front sensor and front wall, distance from right sensor to right wall)
-window.bind('g', lambda event: wrapperFunction())
+def drawTracers(tracerLength):
+    # uses pixel endpts to draw lines to visualize the lengths each number represents
 
-# When 'h' is pressed toggles on/off showing distance labels.
-window.bind('h', lambda event: distLabelsToggle())
+    for lineEndPts in distanceData[2]:
+        # Coordinates of the line's start and end points
+        xPos = int(posXY[0]) + 10
+        yPos = int(posXY[1]) + 10
+        x1, y1 = (xPos), (yPos)  # Starting point (x1, y1)
+        x2, y2 = lineEndPts[0], lineEndPts[1]  # Ending point (x2, y2)
+        print("Current roomba pos:", xPos, yPos)
+        print("Current arrow pos:", x2, y2)
+
+        if tracerLength == "long":
+            arrowEndpointDelta = 1  # value to move arrow endpoint inwards towards, so ti deosnt touch the edge of the map
+        elif tracerLength == "short" and x2 > 25 and y2 > 25:
+            arrowEndpointDelta = 25
+            if x2 > xPos:
+                x2 -= arrowEndpointDelta
+            if x2 < xPos:
+                x2 += arrowEndpointDelta
+            if y2 > yPos:
+                y2 -= arrowEndpointDelta
+            if y2 < yPos:
+                y2 += arrowEndpointDelta
+
+        # Draw a line from (x1, y1) to (x2, y2)
+        lineID = canvas.create_line(x1, y1, x2, y2, fill="blue", width=1, dash=(4, 2), arrow=tk.LAST)
+        lineList.append(lineID)
+
+# Bind the 'g' key using lambda to get distances anytime g is pressed
+# tuple printed in terminal is in the order of (distance between left sensor and left wall,
+# distance from front sensor and front wall, distance from right sensor to right wall)
+window.bind('g', lambda event: drawTracersAndDistancesWrapper())
+window.bind('h', lambda event: toggleDistanceText())
 
 # Set focus on the window to receive key events
 window.focus_set()
